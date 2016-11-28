f9fecd1974abab0380e89ca5fa4fb8987991eb4e
Andrey Popp <8mayday@gmail.com>
2016-11-28 12:45:09 +0300

    feat: initial commit

---
+++ example/webpack.config.js
@@ -0,0 +1,14 @@
+var WebpackRPC = require('../lib/index');
+var WatchIgnorePlugin = require('webpack/lib/WatchIgnorePlugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname + '/bundle',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new WatchIgnorePlugin([/.*/]),
+    new WebpackRPC()
+  ]
+};
