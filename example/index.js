4e21804a900f92a5f6907cda36a18de8653ef759
Andrey Popp <8mayday@gmail.com>
2016-11-28 13:12:26 +0300

    feat: initial commit

--- example/index.js
+++ example/index.js
@@ -1,1 +1,17 @@
 console.log('ok');
+
+console.log('ok');
+
+console.log('ok');
+
+console.log('ok');
+
+console.log('ok');
+
+require('react');
+
+console.log('ok');
+
+console.log('ok');
+
+require('react');
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
-    new WatchIgnorePlugin([/.*/]),
     new WebpackRPC()
   ]
 };
